dcf5959a93968102e99418be6fc6ab9ed76acd1c
meles <meleb4@gmail.com>
2020-12-15 19:25:01 -0500

    update

---
+++ build/precache-manifest.bbb7ef6cbdab66807fec95f66cb8d525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f242ef4ed91f563f51b93558fdf4d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9986104133a4ac0dd3d8",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9986104133a4ac0dd3d8",
+    "url": "/static/js/main.c75c494f.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
